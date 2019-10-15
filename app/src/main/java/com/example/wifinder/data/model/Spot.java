151ce0397e720912ff7c4a974fbd589a41232d8e
Takamatsu679 <17jz0127@jec.ac.jp>
2019-10-15 10:55:17 +0900

    Update Spot.java

--- app/src/main/java/com/example/wifinder/data/model/Spot.java
+++ app/src/main/java/com/example/wifinder/data/model/Spot.java
@@ -5,18 +5,29 @@ public class Spot {
     private int id;
     
     public Spot(int id, String username, String email) {
+    private String spotname;
+    private double longitude, latitude; // 緯度　経度
+
+    public Spot(int id, String spotName, double longitude, double latitude) {
         this.id = id;
-        this.username = username;
-        this.email = email;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public String getEmail() {
-        return email;
+        this.spotname = spotName;
+        this.longitude = longitude;
     }
 
     public int getId() {
         return id;
     }
+
+    public String getSpotname() {
+        return spotname;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
 }
