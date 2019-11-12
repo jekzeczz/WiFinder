68deb128b9f31f2b6bc04a4e8ec7d9112a58980c
Eunkyung Ji <jekzeczz94@gmail.com>
2019-11-12 12:31:24 +0900

    Fragmentの追加
    何もできなかった

--- app/src/main/java/com/example/wifinder/MapsActivity.java
+++ app/src/main/java/com/example/wifinder/MapsActivity.java
@@ -9,12 +9,15 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,140 +26,49 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
-
-    private GoogleMap mMap;
-    private LocationManager locationManager;
-
-    static final int REQUEST_PERMISSION = 1000;
-
-    private double lat;
-    private double lng;
+public class MapsActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        /*
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.nav_view);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+         */
+        //Fragment mapsFragment = new MapsFragment();
+        //loadFragment(mapsFragment);
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        UiSettings us = mMap.getUiSettings();
 
-        //Permission拒否
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, REQUEST_PERMISSION);
-
-            return;
-        }
-        //Permission許可
-        else {
-            locationStart();
-
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 50, this);
-        }
-
-        us.setZoomControlsEnabled(true);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_PERMISSION) {
-            // 使用が許可された
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Log.d("#", "checkSelfPermission true");
-
-                locationStart();
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment fragment;
+            switch (menuItem.getItemId()) {
+                case R.id.navigation_favorite:
+                    Log.e("#", "1111");
+                    fragment = new FavoriteFragment();
+                    loadFragment(fragment);
+                    return true;
             }
-            else {
-                // それでも拒否された時の対応
-                Toast toast = Toast.makeText(this,
-                        "これ以上なにもできません", Toast.LENGTH_SHORT);
-                toast.show();
-            }
+            Log.e("#", "2222");
+            return false;
         }
-    }
+    };
 
-    @Override
-    public void onLocationChanged(Location location) {
-        // 緯度
-        lat = location.getLatitude();
-        lng = location.getLongitude();
-        Log.e("#", "緯度 Latitude:" + location.getLatitude());
-        // 経度
-        Log.e("#", "経度 Longitude:" + location.getLongitude());
-
-        // Latitude:35.775315
-        // Longitude:139.79716333333332
-        //LatLng myLocation = new LatLng(lat, lng);
-        //mMap.addMarker(new MarkerOptions().position(myLocation));
-
-        //現在位置表示
-        mMap.setMyLocationEnabled(true);
-
-        //ズーム状態で表示
-        zoomMap(lat, lng);
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
-
-    private void locationStart() {
-        // LocationManager インスタンス生成
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        if (locationManager != null && locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            Log.e("#", "location manager Enabled");
-        } else {
-            // GPSを設定するように促す
-            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            startActivity(settingsIntent);
-            Log.e("#", "not gpsEnable, startActivity");
-        }
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, REQUEST_PERMISSION);
-            Log.d("#", "checkSelfPermission false");
-            return;
-        }
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, REQUEST_PERMISSION, 50, this);
-    }
-
-    private void zoomMap(double latitude, double longitude){
-        // 表示する東西南北の緯度経度を設定
-        double south = latitude * (1-0.00005);
-        double west = longitude * (1-0.00005);
-        double north = latitude * (1+0.00005);
-        double east = longitude * (1+0.00005);
-
-        // LatLngBounds (LatLng southwest, LatLng northeast)
-        LatLngBounds bounds = LatLngBounds.builder()
-                .include(new LatLng(south , west))
-                .include(new LatLng(north, east))
-                .build();
-
-        int width = getResources().getDisplayMetrics().widthPixels;
-        int height = getResources().getDisplayMetrics().heightPixels;
-
-        // static CameraUpdate.newLatLngBounds(LatLngBounds bounds, int width, int height, int padding)
-        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, width, height, 0));
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        Log.e("#", "3333");
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
