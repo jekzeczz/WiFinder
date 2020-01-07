8e496649e2023d489c685cc1f39f5bb04212802e
jekzeczz <jekzeczz94@gmail.com>
2020-01-07 15:03:38 +0900

    ログインの場合のみプロフィール表示

--- app/src/main/java/com/example/wifinder/SignUpActivity.java
+++ app/src/main/java/com/example/wifinder/SignUpActivity.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
