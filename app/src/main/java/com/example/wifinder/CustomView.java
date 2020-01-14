9505248056a1db85e9a559477bf9f2e34b139eb9
jekzeczz <jekzeczz94@gmail.com>
2020-01-14 14:42:50 +0900

    rating dialogの追加とTODO追加

--- app/src/main/java/com/example/wifinder/CustomView.java
+++ app/src/main/java/com/example/wifinder/CustomView.java
@@ -1,23 +1,27 @@
 package com.example.wifinder;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.wifinder.data.model.Spots;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+
+import com.example.wifinder.data.model.Spots;
 
 public class CustomView extends FrameLayout {
 
     public Spots spot;
     public TextView nameView;
     public TextView addressView;
+    public float ratingValue = 0.0F;
 
     public CustomView(@NonNull Context context) {
         super(context);
@@ -40,12 +44,13 @@ public class CustomView extends FrameLayout {
         nameView = findViewById(R.id.name_view);
         addressView = findViewById(R.id.address_view);
 
+        // TODO: 一回評価したユーザーはボタンを表示しないようにするとか？
         Button reviewButton = view.findViewById(R.id.review_button);
         reviewButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO: Dialogを出す
-                Toast.makeText(context, getSpot().id.toString(), Toast.LENGTH_SHORT).show();
+                // 星で評価するダイアログを出す
+                showDialog(context);
             }
         });
     }
@@ -61,5 +66,33 @@ public class CustomView extends FrameLayout {
 
     public Spots getSpot() {
         return this.spot;
+    }
+
+    public void showDialog(final Context context) {
+        // dialog のレイアウト作成
+        final View customView = inflate(context, R.layout.rating_layout, null);
+        // dialog にレイアウトセット
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+        dialog.setTitle("Wifiはいかがでしたか？");
+        dialog.setView(customView);
+
+        // 点数を取得して変数に入れておく
+        RatingBar ratingBar = customView.findViewById(R.id.rating_bar);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                ratingValue = v;
+            }
+        });
+
+        // 評価するボタンを押した時
+        dialog.setPositiveButton("評価する", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // TODO: ratingValue をDBに保存する
+                Toast.makeText(context, ratingValue + "点", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
