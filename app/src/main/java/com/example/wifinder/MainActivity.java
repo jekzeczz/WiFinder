9caebb96c832208426b7d069f9bb5b1f12d222c8
Eunkyung Ji <jekzeczz94@gmail.com>
2020-01-20 15:02:37 +0900

    ボタンを丸めにした

--- app/src/main/java/com/example/wifinder/MainActivity.java
+++ app/src/main/java/com/example/wifinder/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
