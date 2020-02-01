1e6566d6737f5da0a2da412e875a5cad99af8228
lxeh2008 <56054220+lxeh2008@users.noreply.github.com>
2020-02-01 19:24:45 +0900

    text1

--- app/src/main/java/com/example/wifinder/CustomView.java
+++ app/src/main/java/com/example/wifinder/CustomView.java
@@ -33,19 +33,21 @@ import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import static com.example.wifinder.MainActivity.queren;
+
 public class CustomView extends FrameLayout {
 
     private Spots spot;
     private TextView nameView;
     private TextView addressView;
     private RatingBar ratingBar;
-
+    private ImageButton we;
     private float ratingValue = 0.0F;
     final private Integer FAV_YES = 1;
     final private Integer FAV_NO = 0;
     public Integer dbSpotId;
     public Integer dbIsFavorite;
-
+    public  mSurfaceView m;
     private Integer spotId;
     private String spotName;
     private String spotAddress;
@@ -78,7 +80,13 @@ public class CustomView extends FrameLayout {
         nameView = findViewById(R.id.name_view);
         addressView = findViewById(R.id.address_view);
         ratingBar = findViewById(R.id.rating_bar);
-
+        we = findViewById(R.id.we);
+        we.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                queren = 1;
+            }
+        });
         // TODO: 一回評価したユーザーはボタンを表示しないようにするとか？
         ImageButton reviewButton = view.findViewById(R.id.review_button);
         reviewButton.setOnClickListener(new OnClickListener() {
