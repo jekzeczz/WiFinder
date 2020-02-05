df9f7be4f8d9ec482dee249ce59fd799b8562604
Eunkyung Ji <jekzeczz94@gmail.com>
2020-02-05 10:50:37 +0900

    いらないクラス削除

--- app/src/main/java/com/example/wifinder/data/URLS.java
+++
@@ -1,9 +0,0 @@
-package com.example.wifinder.data;
-
-public class URLS {
-    private static final String URL_ROOT = "http://10.210.20.168/api.php?action=";
-    public static final String URL_SIGNUP = URL_ROOT + "signup";
-    public static final String URL_LOGIN= URL_ROOT + "login";
-
-    public static final String SPOT_ROOT = "http://10.210.20.168/spot.php";
-}
